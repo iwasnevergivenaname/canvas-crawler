60b4df4e9c801da887996b5b5a08c878c09d8248
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-07-24 11:05:05 -0700

    [main.js] hero can move up with W keypress and won't go above board

--- js/main.js
+++ js/main.js
@@ -4,24 +4,13 @@ console.log("hey");
 let movementDisplay = document.getElementById("movement");
 
 // canvas
-let  game = document.getElementById("game");
+let game = document.getElementById("game");
 // set some canvas configs
 game.setAttribute("height", 400);
 game.setAttribute("width", 800);
 
 // context
 let ctx = game.getContext("2d");
-// draw a box
-// set a fill color
-// ctx.fillStyle = "white";
-// set line color
-// ctx.strokeStyle = "red";
-// set line width
-// ctx.lineWidth = 5;
-// call the fillRect to draw the fill square
-// ctx.fillRect(10, 10, 100, 100);
-// call strokeRect to draw the line rectangle
-// ctx.strokeRect(10, 10, 100, 100);
 
 // drawBox func
 function drawBox(x, y, size, color) {
@@ -37,7 +26,7 @@ function Crawler(x, y, w, h, color) {
     this.h = h;
     this.color = color;
     this.alive = true;
-    this.render = function() {
+    this.render = function () {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.w, this.h);
     }
@@ -46,6 +35,31 @@ function Crawler(x, y, w, h, color) {
 // character refs
 let ogre = new Crawler(300, 100, 80, 120, "chartreuse");
 let hero = new Crawler(20, 100, 50, 50, "hotpink");
+
+// game loop
+function gameLoop() {
+//    clear canvas
+    ctx.clearRect(0, 0, game.width, game.height);
+//    display x y co-ords of hero to DOM
+    movementDisplay.textContent = `x: ${hero.x} & y: ${hero.y}`;
+//    check if ogre is alive
+    if (ogre.alive)  {
+        ogre.render();
+    }
+//    render hero
+    hero.render();
+}
+
+// movement
+function movementHandler(e) {
+    console.log("e");
+//    user presses W,
+    if (e.keyCode === 87 && hero.y > 0) {
+        //decrease heroes y co-ord
+        hero.y -= 5;
+    }
+//    hero moves up
+}
 
 // shows green ogres where you click
 game.addEventListener("click", (e) => {
@@ -60,3 +74,6 @@ game.addEventListener("click", (e) => {
     ogre.render();
 });
 
+document.addEventListener("keydown", movementHandler);
+
+let runGame = setInterval(gameLoop, 60);
